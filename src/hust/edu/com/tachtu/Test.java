3290b4e642f52f9129c64fde6bd99580c03d4ff9
cuongnv <nvcuongbkhnict@gmail.com>
2015-01-31 08:31:18 +0700

    ok

--- src/hust/edu/com/tachtu/Test.java
+++ src/hust/edu/com/tachtu/Test.java
@@ -1,8 +1,11 @@
 package hust.edu.com.tachtu;
 
+import vn.hus.nlp.tokenizer.VietTokenizer;
+
 public class Test {
 	public static void main(String[] args){
 //		PennTokenizer.tokenize("ha noi pho");
 		System.err.println("ok");
+		VietTokenizer tokenizer = new VietTokenizer();
 	}
 }
