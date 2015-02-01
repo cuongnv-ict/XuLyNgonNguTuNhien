ac08d8025aae1383625c3c1c42aa4c233733e3e9
cuongnv <nvcuongbkhnict@gmail.com>
2015-02-01 07:08:35 +0700

    ok

--- src/hust/edu/com/tachtu/Test.java
+++ src/hust/edu/com/tachtu/Test.java
@@ -1,15 +1,20 @@
 package hust.edu.com.tachtu;
 
+import jvntextpro.JVnTextPro;
 import vn.hus.nlp.tokenizer.VietTokenizer;
 
 public class Test {
 	public static void main(String[] args){
-//		PennTokenizer.tokenize("ha noi pho");
-		System.err.println("ok");
 		VietTokenizer tokenizer = new VietTokenizer();
-//		String []str = tokenizer.tokenize("Ha Noi pho");
-//		for(int i =0;i<str.length;i++){
-//			System.err.println(str[i]);
-//		}
+		String []str = tokenizer.tokenize("Ha Noi      pho, gio thu");
+		JVnTextPro textPro = new JVnTextPro();
+		textPro.initSenSegmenter("JVnTextPro-v.2.0/models/jvnsensegmenter");
+		textPro.initSenTokenization();
+		textPro.initSegmenter("JVnTextPro-v.2.0/models/jvnsegmenter");
+		textPro.initPosTagger("JVnTextPro-v.2.0/models/jvnpostag/maxent");
+		System.err.println(textPro.process("chim cu"));
+		for(int i =0;i<str.length;i++){
+			System.err.println(str[i]);
+		}
 	}
 }
